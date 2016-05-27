16028650c4ba644bc78c60e612278988668e0350
qzpm <tarako2000yen@yahoo.co.jp>
2016-05-28 01:59:42 +0900

    Renew js

--- js/source.js
+++ js/source.js
@@ -1,40 +1,33 @@
 $(function() {
+  "use strict";
+
   $('body').on('contextmenu', function() {
     alert("右クリック禁止!!!")
-    return false
   })
-})
 
-$(function() {
   $('html').mousemove(function(e) {
-    var dx = e.pageX
-    var dy = e.pageY
+    const dx = e.pageX
+    const dy = e.pageY
     $('.stalker').css('top', dy - 80).css('left', dx + 50)
   })
-})
 
-$(function() {
-  var i = 0
-  var colorset = ['black', 'blue', 'green', 'yellow']
-  var title = $('.marquee')
-  var blink = $('.blink')
-  setInterval(function() {
-    i++
-    title.css('color', colorset[i % 4])
-    if(blink.css('visibility') == 'visible') {
-      blink.css('visibility', 'hidden')
-    } else {
-      blink.css('visibility', 'visible')
-    }
-  }, 500)
-})
-
-$(function() {
-  var test = $('.test')
-  var content = $('.content')
-  content.css('display', 'none')
-  test.click(function() {
+  $('.test').click(function() {
     $('.enter').css('display', 'none')
-    content.css('display', 'inline')
+    $('.content').css('display', 'flex')
+  })
+
+  $(function() {
+    let i = 0
+    const colorset = ['black', 'blue', 'green', 'yellow']
+    const blink = $('.blink')
+    setInterval(function() {
+      i++
+      $('.marquee').css('color', colorset[i % 4])
+      if(blink.css('visibility') == 'visible') {
+        blink.css('visibility', 'hidden')
+      } else {
+        blink.css('visibility', 'visible')
+      }
+    }, 500)
   })
 })
