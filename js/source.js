fbcd0d27be1b0bd8fe87a701a5acc40b9861fabc
qzpm <tarako2000yen@yahoo.co.jp>
2016-06-13 03:31:53 +0900

    Refactor

--- js/source.js
+++ js/source.js
@@ -3,26 +3,15 @@ $(() => {
 
   $('body').on('contextmenu', () => alert("右クリック禁止!!!"))
 
-  $('html').mousemove(e => {
-    const dx = e.pageX
-    const dy = e.pageY
-    const stalker = $('.stalker')
-    const image_height = stalker.height()
-    const image_width = stalker.width()
-    const c_width = $('.content').width()
-    const c_height = $('.content').height()
-
-    if(c_width <= dx + image_width) {
-      stalker.css('left', c_width - image_width)
-    } else {
-      stalker.css('left', dx + 30)
-    }
-
-    if(c_height <= dy + image_height / 2) {
-      stalker.css('top', c_height - image_height)
-    } else {
-      stalker.css('top', dy - image_height / 2)
-    }
+  $(() => {
+    let i = 0
+    const colorset = ['black', 'blue', 'green', 'yellow']
+    const marquee = $('.marquee')
+    const blink = $('.blink')
+    setInterval(() => {
+      marquee.css('color', colorset[i++ % 4])
+      blink.toggle()
+    }, 500)
   })
 
   $('.door').click(() => {
@@ -30,18 +19,14 @@ $(() => {
     $('.content').css('display', 'flex')
   })
 
-  $(() => {
-    let i = 0
-    const colorset = ['black', 'blue', 'green', 'yellow']
-    const blink = $('.blink')
-    const marquee = $('.marquee')
-    setInterval(() => {
-      marquee.css('color', colorset[i++ % 4])
-      if(blink.css('visibility') == 'visible') {
-        blink.css('visibility', 'hidden')
-      } else {
-        blink.css('visibility', 'visible')
-      }
-    }, 500)
+  $('html').mousemove(e => {
+    const [dx, dy] = [e.pageX, e.pageY]
+    const [img_h, img_w] = [$('.stalker').height(), $('.stalker').width()]
+    const [c_w, c_h] = [$('.content').width(), $('.content').height()]
+
+    const img_x = ((c_w <= dx + img_w)? c_w - img_w : dx + 30)
+    const img_y = ((c_h <= dy + img_h / 2)? c_h - img_h : dy - img_h / 2)
+
+    $('.stalker').css('left', img_x).css('top', img_y)
   })
 })
